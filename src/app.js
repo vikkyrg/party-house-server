b8f8e2da92d690e318828e05ec3e0c3546e0b9dd
vikkyrg <rvikky05@gmail.com>
2026-09-16 17:25:36 +0530

    fix: allow all CORS origins

--- src/app.js
+++ src/app.js
@@ -17,10 +17,9 @@ app.set('trust proxy', 1);
 
 app.use(helmet());
 
-const corsOrigins = parseCorsOrigins(process.env.CORS_ORIGIN);
 app.use(
   cors({
-    origin: corsOrigins === '*' ? true : corsOrigins,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
